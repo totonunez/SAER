7e6047b8b55ff1965933048bfe0cd3f70f0babf1
cr-urra <cr.urra6@gmail.com>
2020-10-25 00:51:34 -0300

    Inicio de frontend en react, configuracion de react-router, instalación de bootstrap y se añaden componentes del navbar y el login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigation from './components/Navigation';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div>
-      <p>hello world!</p>
-    </div>
+    <BrowserRouter>
+      <Route path="/login" component={Login}/>
+      <Route path="/navigation" component={Navigation}/>
+    </BrowserRouter>
   );
 }
 
