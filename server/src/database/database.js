f31e0cfba2aea9d820e59411d0b155dfeb39b352
cr-urra <cr.urra6@gmail.com>
2020-11-26 19:17:22 -0300

    se sube configuracion de bd (no cambiar)

---
+++ server/src/database/database.js
@@ -0,0 +1,19 @@
+import sequelize from 'sequelize';
+
+export const database = new sequelize(
+    'saer01', //Nombre bd
+    'saer', //Nombre bd
+    'postgres', //Usuario
+    'pass123', //Contraseña
+    {
+        host: 'localhost',
+        dialect: 'postgres',
+        pool:{
+            max: 5,
+            min: 0,
+            require: 30000,
+            idle: 1000
+        },
+        logging: false
+    }
+)
