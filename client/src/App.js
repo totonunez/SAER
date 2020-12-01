da2ea42987c21178cc1065ebaa3d3c8ebca2db4a
Benjamín Morales <benjamin.morales1997@gmail.com>
2020-12-01 13:07:48 -0300

    Update App.js
    App final

--- client/src/App.js
+++ client/src/App.js
@@ -5,32 +5,59 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Init from './components/Init';
 import AdminInterface from './components/AdminInterface';
-import AdminReclamos from './components/AdminReclamos';
 import SuperUserInterface from './components/SuperUserInterface';
 import UserInterface from './components/UserInterface';
-import UserHome from './components/Userhome';
 import AdminGastosGomunes from './components/AdminGastosComunes';
-import UserGastosComunes from './components/UserGastosComunes.js';
-import UserBodegaReparacion from './components/UserBodegaReparacion';
+import AdminBodega from './components/AdminBodega';
+import AdminReclamos from './components/AdminReclamos';
+import AdminGastosComunesCrear from './components/AdminGastosComunesCrear';
+import AdminGastosComunesRevisar from './components/AdminGastosComunesRevisar';
+import AdminGastosComunesCuenta from './components/AdminGastosComunesCuenta';
+import AdminGastosComunesHistorial from './components/AdminGastosComunesHistorial';
+import UserReclamos from './components/UserReclamos';
+import UserGastosComunes from './components/UserGastosComunes';
+import UserReclamosCrear from './components/UserReclamosCrear';
+import UserReclamosRevisar from './components/UserReclamosRevisar';
+import UserGastosComunesVer from './components/UserGastosComunesVer';
+import AdminUsuarios from './components/AdminUsuarios';
+import AdminUsuariosCrear from './components/AdminUsuariosCrear';
+import AdminUsuariosEditar from './components/AdminUsuariosEditar';
+import AdminUsuariosEliminar from './components/AdminUsuariosEliminar';
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Init}/>
       <Route path="/users/adm" exact component={AdminInterface}/>
+      <Route path="/users/adm/usuarios" exact component={AdminUsuarios}/>
+      <Route path="/users/adm/usuarios/crear" exact component={AdminUsuariosCrear}/>
+      <Route path="/users/adm/usuarios/editar" exact component={AdminUsuariosEditar}/>
+      <Route path="/users/adm/usuarios/eliminar" exact component={AdminUsuariosEliminar}/>
+      <Route path="/users/adm/bodega" exact component={AdminBodega}/>
       <Route path="/users/adm/reclamos" exact component={AdminReclamos}/>
-    
+      <Route path="/users/adm/gastos" exact component={AdminGastosGomunes}/>
+      <Route path="/users/adm/gastos/crear" exact component={AdminGastosComunesCrear}/>
+      <Route path="/users/adm/gastos/revisar" exact component={AdminGastosComunesRevisar}/>
+      <Route path="/users/adm/gastos/cuenta" exact component={AdminGastosComunesCuenta}/>
+      <Route path="/users/adm/gastos/cuenta/historial" exact component={AdminGastosComunesHistorial}/>
+
+
       <Route path="/users/sup" exact component={SuperUserInterface}/>
+
+
       <Route path="/users/usr" exact component={UserInterface}/>
-
-    
-    
-      <Route path="/admin/gastos" exact component={AdminGastosGomunes}/>
-
-      <Route path="/users/home" exact component={UserHome}/>
-      <Route path="/gastoscomunes" exact component={UserGastosComunes}/>
-      <Route path="/users/bodegareparacion" exact component={UserBodegaReparacion}/>
+      <Route path="/users/usr/reclamos" exact component={UserReclamos}/>
+      <Route path="/users/usr/reclamos/crear" exact component={UserReclamosCrear}/>
+      <Route path="/users/usr/reclamos/revisar" exact component={UserReclamosRevisar}/>
+      <Route path="/users/usr/gastos" exact component={UserGastosComunes}/>
+      <Route path="/users/usr/gastos/ver" exact component={UserGastosComunesVer}/>
       
+
+
+
+      
+
+ 
     </BrowserRouter>
   );
 }
